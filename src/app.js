0e025974d7b3353a54ec08f6b81853dfa997783b
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-13 09:13:08 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+import express from "express";
+import cors from "cors";
+import { chatHandler } from "./controllers/chatController.js";
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend PRO funcionando 🚀");
+});
+
+app.post("/chat", chatHandler);
+
+export default app;
